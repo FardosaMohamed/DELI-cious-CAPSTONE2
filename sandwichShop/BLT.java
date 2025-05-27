fe30ac0b7f3e7fb1f2188b98cbff406c018440af
fardosamohamed <Fardosa.Mohamed2@seattlecolleges.edu>
2025-05-27 14:18:01 -0700

    wip

---
+++ sandwichShop/BLT.java
@@ -0,0 +1,5 @@
+public class BLT extends Sandwich{
+    public BLT(int size, String typeOfBread, boolean isToasted) {
+        super(size, typeOfBread, isToasted);
+    }
+}
---
+++ sandwichShop/PhillyCheeseSteak.java
@@ -0,0 +1,5 @@
+public class PhillyCheeseSteak extends Sandwich {
+    public PhillyCheeseSteak(int size, String typeOfBread, boolean isToasted) {
+        super(size, typeOfBread, isToasted);
+    }
+}
